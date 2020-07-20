aaa48dd7ac42910bd41042a1b1a0f1efe6db0375
Martyna Chojnowska <martynachojnowska12@gmail.com>
2020-07-20 23:58:48 +0200

    Add new endpoints and tests to them

--- models/concert.model.js
+++ models/concert.model.js
@@ -5,7 +5,7 @@ const concertSchema = new mongoose.Schema({
     genre: {type: String, required: true},
     price: {type: Number, required: true},
     day: {type: Number, required: true},
-    image: {type: String, required: true}
+    image: {type: String, required: false}
 });
 
 module.exports = mongoose.model('Concert', concertSchema);
--- test/api/concerts/get.test.js
+++ test/api/concerts/get.test.js
@@ -9,7 +9,7 @@ const Concert = require('../../../models/concert.model');
 
 describe('GET /api/concerts', () => {
     before(async () => {
-        const testConcertOne = new Concert ({_id: '5d9f1140f10a81216cfd4408', performer: 'Shakira', genre: 'POP', price: 25, day: 1});
+        const testConcertOne = new Concert ({_id: '5d9f1140f10a81216cfd4408', performer: 'Shakira', genre: 'POP', price: 5, day: 1});
         await testConcertOne.save();
 
         const testConcertTwo = new Concert ({_id: '5d9f1140f10a81216cfd5487', performer: 'Ricky Martin', genre: 'POP', price: 25, day: 2});
@@ -19,18 +19,27 @@ describe('GET /api/concerts', () => {
         await Concert.deleteMany();
     });
     it('should return concerts by performer', async () => {
-        const res = await request(server).get('/api/departments/5d9f1140f10a81216cfd4408');
+        const res = await request(server).get('/api/concerts/performer/Shakira');
         expect(res.status).to.be.equal(200);
         expect(res.body).to.be.an('array');
-        expect(res.body).to.not.be.null;
+        expect(res.body.length).to.be.equal(1);
     });
     it('should return concerts by genre', async () => {
-
+        const res = await request(server).get('/api/concerts/genre/POP');
+        expect(res.status).to.be.equal(200);
+        expect(res.body).to.be.an('array');
+        expect(res.body.length).to.be.equal(2);
     });
     it('should return concerts by price', async () => {
-
+        const res = await request(server).get('/api/concerts/price/10/30');
+        expect(res.status).to.be.equal(200);
+        expect(res.body).to.be.an('array');
+        expect(res.body.length).to.be.equal(1);
     });
     it('should return concerts by day', async () => {
-
+        const res = await request(server).get('/api/concerts/day/1');
+        expect(res.status).to.be.equal(200);
+        expect(res.body).to.be.an('array');
+        expect(res.body.length).to.be.equal(1);
     });
 });
